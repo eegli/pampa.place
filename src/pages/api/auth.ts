90a1ca6b3d7bcb34522c7f1ec4ccf8b3a3556e34
Eric <43848365+eegli@users.noreply.github.com>
2021-09-21 23:17:56 +0200

    simplify secrets

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -10,24 +10,13 @@ export type AuthRes = {
   apikey: string;
 };
 
-const accessPW = env.APP_ACCESS_PW || '';
-
-function getApiKeyFromEnv() {
-  switch (env.NODE_ENV) {
-    case 'development':
-      return env.MAPS_LOCAL_API_KEY || '';
-    case 'production':
-      return env.MAPS_PROD_API_KEY || '';
-    default:
-      return '';
-  }
-}
+const accessPW = env.APP_ACCESS_PW;
+const envApiKey = env.MAPS_API_KEY;
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<AuthRes>
 ) {
-  const envApiKey = getApiKeyFromEnv();
   if (!envApiKey) {
     throw new Error('Missing API keys');
   }
