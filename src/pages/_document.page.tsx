2b1ff5d599d069a9da7e6d1a5912894562dbd9c0
Eric <43848365+eegli@users.noreply.github.com>
2021-11-07 11:49:07 +0100

    improve font loading

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -23,12 +23,12 @@ export default class MyDocument extends Document {
 
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=optional"
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@100;400&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@100;400&display=optional"
           rel="stylesheet"
         ></link>
 
