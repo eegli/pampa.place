2947a5db1afc59dead06b9ae2e7c19866be19fee
Eric <43848365+eegli@users.noreply.github.com>
2021-11-24 00:50:22 +0100

    update tests

--- src/components/google/__tests__/map.test.tsx
+++ src/components/google/__tests__/map.test.tsx
@@ -1,12 +1,19 @@
 import {testMapId} from '@/config/__mocks__/maps';
 import {render, screen} from '@/tests/test-utils';
-import {Map} from '@googlemaps/jest-mocks';
 import React from 'react';
+import {mocked} from 'ts-jest/utils';
 import {Gmap} from '../../../services/google-map';
 import GoogleMap, {MapMode} from '../google.map';
 
 jest.spyOn(React, 'useRef').mockReturnValue({
   current: document.createElement('div'),
+});
+
+const mockGmap = mocked(Gmap, true);
+const mockGoogle = mocked(google.maps, true);
+
+afterEach(() => {
+  jest.clearAllMocks();
 });
 
 describe('Google Map', () => {
@@ -16,24 +23,27 @@ describe('Google Map', () => {
     expect(screen.getByTestId('__GMAP__')).toHaveStyle('height:100%');
   });
   it('has preview mode', () => {
-    const addGeoJsonSpy = jest.spyOn(Map.prototype.data, 'addGeoJson');
-    const setStyleSpy = jest.spyOn(Map.prototype.data, 'setStyle');
-    const removeGeoJsonSpy = jest.spyOn(Map.prototype.data, 'remove');
-    const setOptonsSpy = jest.spyOn(Gmap.map, 'setOptions');
-
     const {unmount} = render(
       <GoogleMap mapId={testMapId} mode={MapMode.PREVIEW} />
     );
-    expect(setOptonsSpy.mock.calls[0]).toMatchSnapshot('preview settings');
-    expect(addGeoJsonSpy).toHaveBeenCalledTimes(1);
-    expect(setStyleSpy).toHaveBeenCalledTimes(1);
+    expect(mockGmap.map.setOptions.mock.calls[0]).toMatchSnapshot(
+      'preview settings'
+    );
+    expect(mockGoogle.Map.prototype.data.setStyle).toHaveBeenCalledTimes(1);
+    expect(mockGoogle.Map.prototype.data.addGeoJson).toHaveBeenCalledTimes(1);
+    expect(mockGoogle.Map.prototype.data.setStyle).toHaveBeenCalledTimes(1);
     unmount();
-    expect(removeGeoJsonSpy).toHaveBeenCalledTimes(3);
+    expect(mockGoogle.Map.prototype.data.remove).toHaveBeenCalledTimes(3);
   });
 
   it('has play mode', () => {
     const {unmount} = render(
       <GoogleMap mapId={testMapId} mode={MapMode.PLAY} />
+    );
+
+    expect(google.maps.event.addListenerOnce).toHaveBeenCalledTimes(1);
+    expect(mockGmap.map.setOptions.mock.calls[0]).toMatchSnapshot(
+      'play settings'
     );
   });
 });
