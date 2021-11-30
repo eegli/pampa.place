34c2202823acb797975d4cb303749642d2e9d0b8
Eric <43848365+eegli@users.noreply.github.com>
2021-11-30 10:37:57 +0100

    wip

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,4 @@
 module.exports = {
   '**/*.(ts|js)?(x)': filenames =>
-    `next lint --file ${filenames
-      .map(file => file.split(process.cwd())[1])
-      .join(' --file ')}`,
+    `eslint ${filenames.map(file => file.split(process.cwd())[1]).join(' ')}`,
 };
