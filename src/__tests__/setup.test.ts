83ce75cc8951356f4a44a5415efb039044516757
Eric <43848365+eegli@users.noreply.github.com>
2021-12-03 16:09:18 +0100

    inline snapshots for setup

--- src/__tests__/setup.test.ts
+++ src/__tests__/setup.test.ts
@@ -1,12 +1,47 @@
 /* 
-There are a lot of Google implementations that are used in the thunks that need to be mocked properly, e.g. StreetViewPreference and StreetViewSource (see jest-setup.ts). Make sure they have a correctly defined initial implementation that can later be overwritten individually for each test. 
+There are a lot of Google implementations that are used in the thunks
+that need to be mocked properly, e.g. StreetViewPreference and
+StreetViewSource (see jest-setup.ts). Make sure they have a correctly
+defined initial implementation that can later be overwritten
+individually for each test. 
 */
 
 describe('Google global mock setup', () => {
   it('street view service resolves', async () => {
     const service = new google.maps.StreetViewService();
     expect(service).toBeTruthy();
-    expect(await service.getPanorama({})).toMatchSnapshot('default response');
+    expect(await service.getPanorama({})).toMatchInlineSnapshot(`
+Object {
+  "data": Object {
+    "copyright": "Copyright",
+    "imageDate": "August 2020",
+    "links": Array [],
+    "location": Object {
+      "description": "Fake panorama description",
+      "latLng": Object {
+        "lat": 0,
+        "lng": 0,
+      },
+      "pano": "69",
+      "shortDescription": null,
+    },
+    "tiles": Object {
+      "centerHeading": 0,
+      "getTileUrl": [Function],
+      "tileSize": Size {
+        "height": 1,
+        "toString": [MockFunction],
+        "width": 1,
+      },
+      "worldSize": Size {
+        "height": 1,
+        "toString": [MockFunction],
+        "width": 1,
+      },
+    },
+  },
+}
+`);
 
     jest
       .spyOn(google.maps.StreetViewService.prototype, 'getPanorama')
@@ -14,13 +49,25 @@ describe('Google global mock setup', () => {
       .mockImplementation(() => {
         return Promise.resolve({ok: true});
       });
-    expect(await service.getPanorama({})).toMatchSnapshot(
-      'overwritten response'
-    );
+    expect(await service.getPanorama({})).toMatchInlineSnapshot(`
+Object {
+  "ok": true,
+}
+`);
   });
   it('enums are defined', () => {
-    expect(google.maps.StreetViewPreference).toMatchSnapshot();
-    expect(google.maps.StreetViewSource).toMatchSnapshot();
+    expect(google.maps.StreetViewPreference).toMatchInlineSnapshot(`
+Object {
+  "BEST": "best",
+  "NEAREST": "nearest",
+}
+`);
+    expect(google.maps.StreetViewSource).toMatchInlineSnapshot(`
+Object {
+  "DEFAULT": "default",
+  "OUTDOOR": "outdoor",
+}
+`);
   });
 });
 
