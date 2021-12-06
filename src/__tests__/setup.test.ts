0ee750d3c7c1465193223600fd2fec0e128c87ad
Eric <43848365+eegli@users.noreply.github.com>
2021-12-06 11:19:25 +0100

    wip

--- src/__tests__/setup.test.ts
+++ src/__tests__/setup.test.ts
@@ -6,72 +6,32 @@ defined initial implementation that can later be overwritten
 individually for each test. 
 */
 
+import {StreetViewService} from './setup';
+
 describe('__Google mock setup__', () => {
-  it('tests', () => {
-    const l = new google.maps.TrafficLayer();
-    expect(l.getMap()).toMatchInlineSnapshot(`"map"`);
-  });
   it('street view service resolves', async () => {
     const service = new google.maps.StreetViewService();
     expect(service).toBeTruthy();
-    expect(await service.getPanorama({})).toMatchInlineSnapshot(`
-Object {
-  "data": Object {
-    "copyright": "Copyright",
-    "imageDate": "August 2020",
-    "links": Array [],
-    "location": Object {
-      "description": "Fake panorama description",
-      "latLng": Object {
-        "lat": 0,
-        "lng": 0,
-      },
-      "pano": "69",
-      "shortDescription": null,
-    },
-    "tiles": Object {
-      "centerHeading": 0,
-      "getTileUrl": [Function],
-      "tileSize": Size {
-        "height": 1,
-        "toString": [MockFunction],
-        "width": 1,
-      },
-      "worldSize": Size {
-        "height": 1,
-        "toString": [MockFunction],
-        "width": 1,
-      },
-    },
-  },
-}
-`);
+    expect(await service.getPanorama({})).toMatchSnapshot();
+    StreetViewService.prototype.getPanorama = jest.fn().mockResolvedValue({
+      ok: true,
+    });
 
-    jest
-      .spyOn(google.maps.StreetViewService.prototype, 'getPanorama')
-      // @ts-ignore
-      .mockImplementation(() => {
-        return Promise.resolve({ok: true});
-      });
-    expect(await service.getPanorama({})).toMatchInlineSnapshot(`
-Object {
-  "ok": true,
-}
-`);
+    expect(await service.getPanorama({})).toMatchSnapshot();
   });
   it('enums are defined', () => {
     expect(google.maps.StreetViewPreference).toMatchInlineSnapshot(`
-Object {
-  "BEST": "best",
-  "NEAREST": "nearest",
-}
-`);
+      Object {
+        "BEST": "best",
+        "NEAREST": "nearest",
+      }
+    `);
     expect(google.maps.StreetViewSource).toMatchInlineSnapshot(`
-Object {
-  "DEFAULT": "default",
-  "OUTDOOR": "outdoor",
-}
-`);
+      Object {
+        "DEFAULT": "default",
+        "OUTDOOR": "outdoor",
+      }
+    `);
   });
 });
 
