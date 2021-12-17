7045f33ca91bd05c424586662171e25798c0bab6
Eric <43848365+eegli@users.noreply.github.com>
2021-12-17 09:42:17 +0100

    don't lie to eslint

--- src/pages/wrappers/auth.tsx
+++ src/pages/wrappers/auth.tsx
@@ -5,42 +5,42 @@ import {useAppSelector} from '../../redux/hooks';
 import {PageContentWrapper} from '../../styles/containers';
 import {Login} from './login';
 
+declare global {
+  interface Window {
+    gm_authFailure: () => void;
+  }
+}
+
 export const AuthWrapper = ({children}: {children?: ReactNode}) => {
   const apiKey = useAppSelector(s => s.app.apiKey);
 
   // https://developers.google.com/maps/documentation/javascript/events
   useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (window as any).gm_authFailure = () => {
-      // Maybe a TODO
+    window.gm_authFailure = () => {
+      console.error('Invalid Google Maps API key: ' + apiKey);
     };
-  });
-
+  }, [apiKey]);
   // An empty string as api key is allowed for development mode
   if (typeof apiKey !== 'string') return <Login />;
 
-  return (
-    <Wrapper
-      apiKey={apiKey}
-      version="3.47.2"
-      render={status => {
-        switch (status) {
-          case Status.SUCCESS:
-            return <>{children}</>;
-          case Status.LOADING:
-            return (
-              <PageContentWrapper id="spinner">
-                <Spinner />
-              </PageContentWrapper>
-            );
-          default:
-            return (
-              <PageContentWrapper id="spinner">
-                <div>mega fail</div>
-              </PageContentWrapper>
-            );
-        }
-      }}
-    />
-  );
+  function render(status: Status) {
+    switch (status) {
+      case Status.SUCCESS:
+        return <>{children}</>;
+      case Status.LOADING:
+        return (
+          <PageContentWrapper id="spinner">
+            <Spinner />
+          </PageContentWrapper>
+        );
+      default:
+        return (
+          <PageContentWrapper id="spinner">
+            <div>mega fail</div>
+          </PageContentWrapper>
+        );
+    }
+  }
+
+  return <Wrapper apiKey={apiKey} version="3.47.2" render={render} />;
 };
