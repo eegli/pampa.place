3c17cd914a8a277f388e9b465695aa547dfb85ff
Eric <43848365+eegli@users.noreply.github.com>
2022-01-17 08:31:29 +0100

    conditional ga

--- src/lib/analytics.tsx
+++ src/lib/analytics.tsx
@@ -1,25 +1,47 @@
+import {useRouter} from 'next/router';
 import Script from 'next/script';
+import {useEffect} from 'react';
 
 const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
-export const GoogleAnalytics = () =>
-  GA_TRACKING_ID ? (
+export const GoogleAnalytics = () => {
+  if (!GA_TRACKING_ID) {
+    return null;
+  }
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      window.gtag('config', GA_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
     <>
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
       />
       <Script
-        id="gtag-init"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${GA_TRACKING_ID}');
-  `,
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${GA_TRACKING_ID}', {
+    page_path: window.location.pathname,
+  });
+`,
         }}
       />
     </>
-  ) : null;
+  );
+};
