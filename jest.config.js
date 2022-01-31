f2fb9a8c9206ef331687390baf135ad969cbc140
Eric <43848365+eegli@users.noreply.github.com>
2022-02-01 00:02:36 +0100

    limit jest workers to 1

--- jest.config.js
+++ jest.config.js
@@ -25,6 +25,7 @@ const config = {
     '^@/tests/(.*)$': '<rootDir>/tests/$1',
   },
   bail: true,
+  maxWorkers: 1,
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   moduleDirectories: ['node_modules', '<rootDir>/'],
   roots: ['<rootDir>/src/', '<rootDir>/tests/'],
