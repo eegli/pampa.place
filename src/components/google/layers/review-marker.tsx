157a96878d6e66c734996e85feee3a43679b2b33
Eric <43848365+eegli@users.noreply.github.com>
2022-02-02 22:06:33 +0100

    cleanup polylines

--- src/components/google/layers/review-marker.tsx
+++ src/components/google/layers/review-marker.tsx
@@ -14,6 +14,7 @@ export const GoogleMapReviewMarkerLayer = () => {
     if (initialPosition) {
       // Add original location marker
       let markers = [new google.maps.Marker()];
+      let polylines: google.maps.Polyline[] = [];
 
       markers[0].setPosition(initialPosition);
       markers[0].setMap(MapService.map);
@@ -21,44 +22,48 @@ export const GoogleMapReviewMarkerLayer = () => {
       // Add marker for each result
       scores.forEach((p, idx) => {
         if (p.selected) {
-          const playerMarker = new google.maps.Marker({
-            position: p.selected,
-            map: MapService.map,
-            label: {
-              text: p.name,
-              color: 'white',
-              className: 'map-marker',
-            },
-            icon: {
-              path: config.marker.svg.path,
-              fillColor: `#${config.marker.colors[idx]}`,
-              fillOpacity: 1,
-              anchor: new google.maps.Point(
-                config.marker.svg.anchor[0],
-                config.marker.svg.anchor[1]
-              ),
-              strokeWeight: 0,
-              scale: 1,
-              labelOrigin: new google.maps.Point(15, 60),
-            },
-          });
-
-          new google.maps.Polyline({
-            path: [initialPosition, p.selected],
-            map: MapService.map,
-            geodesic: true,
-            strokeColor: `#${config.marker.colors[idx]}`,
-            strokeOpacity: 1.0,
-            strokeWeight: 4,
-          });
-          markers.push(playerMarker);
+          markers.push(
+            new google.maps.Marker({
+              position: p.selected,
+              map: MapService.map,
+              label: {
+                text: p.name,
+                color: 'white',
+                className: 'map-marker',
+              },
+              icon: {
+                path: config.marker.svg.path,
+                fillColor: `#${config.marker.colors[idx]}`,
+                fillOpacity: 1,
+                anchor: new google.maps.Point(
+                  config.marker.svg.anchor[0],
+                  config.marker.svg.anchor[1]
+                ),
+                strokeWeight: 0,
+                scale: 1,
+                labelOrigin: new google.maps.Point(15, 60),
+              },
+            })
+          );
+          polylines.push(
+            new google.maps.Polyline({
+              path: [initialPosition, p.selected],
+              map: MapService.map,
+              geodesic: true,
+              strokeColor: `#${config.marker.colors[idx]}`,
+              strokeOpacity: 1.0,
+              strokeWeight: 4,
+            })
+          );
         }
       });
       // Cleanup is important!
       // https://developers.google.com/maps/documentation/javascript/examples/marker-remove
       return () => {
         markers.forEach(m => m.setMap(null));
+        polylines.forEach(p => p.setMap(null));
         markers = [];
+        polylines = [];
       };
     }
   }, [scores, initialPosition]);
