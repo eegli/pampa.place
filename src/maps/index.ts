e83b75875e6f5805f3f3da450f9a202dadca5b0f
Eric Egli <43848365+eegli@users.noreply.github.com>
2023-01-25 19:50:03 +0100

    wip

--- src/maps/index.ts
+++ src/maps/index.ts
@@ -2,15 +2,11 @@ import {generateMapData} from './helpers/generator';
 
 export const MAPS = generateMapData(
   {
-    collection: require('maps/ch-cantons.json'),
-    category: 'Swiss Cantons',
+    collection: require('geojson/che-cantons.json'),
+    category: 'Switzerland',
   },
   {
-    collection: require('maps/us-states.json'),
-    category: 'US States',
-  },
-  {
-    collection: require('maps/countries.json'),
-    category: 'Countries',
+    collection: require('geojson/usa-states.json'),
+    category: 'USA',
   }
 );
