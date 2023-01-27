27c8d90172313983a2fa9fc872dcc7f0be46942c
Eric Egli <43848365+eegli@users.noreply.github.com>
2023-01-27 09:11:05 +0100

    improve login screen

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -4,6 +4,7 @@ import {PageContent} from '@/styles/containers';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 import IconButton from '@mui/material/IconButton';
 import Link from '@mui/material/Link';
 import TextField from '@mui/material/TextField';
@@ -17,19 +18,24 @@ export const Login = () => {
   const [inputError, setInputError] = useState<string>('');
   const [serverPassword, setServerPassword] = useState<string>('');
   const [userApiKey, setUserApiKey] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const dispatch = useAppDispatch();
 
   async function handleSubmit() {
+    setIsLoading(true);
     if (!serverPassword && !userApiKey) {
       setInputError('Either a password or API key must be provided');
+      setIsLoading(false);
     } else if (userApiKey) {
       dispatch(setApiKey(userApiKey));
+      setIsLoading(false);
     } else if (serverPassword) {
       const params: AuthReq = {
         pw: serverPassword,
       };
       try {
+        await new Promise(r => setTimeout(r, 1000));
         const res: AuthRes = await (
           await fetch(
             `${window.location.origin}/api/auth?` + new URLSearchParams(params)
@@ -46,6 +52,8 @@ export const Login = () => {
           eventName: 'login_password_failed',
           category: 'login',
         });
+      } finally {
+        setIsLoading(false);
       }
     }
   }
@@ -99,7 +107,18 @@ export const Login = () => {
         padding={4}
         maxWidth={500}
       >
+        <Box my={2} alignSelf="flex-start">
+          <Typography mb={2} variant="h4" color="text.primary">
+            Login
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            For the best experience, either login with the password that the
+            host of this server gave you&nbsp;<b>or</b>&nbsp;bring your own
+            Google Maps API key.
+          </Typography>
+        </Box>
         <TextField
+          sx={{my: 2}}
           fullWidth
           error={!!inputError}
           helperText={inputError}
@@ -111,9 +130,9 @@ export const Login = () => {
           onChange={handlePasswordInput}
           onKeyUp={handleKeyUp}
         />
-        <Typography component="p" alignSelf="flex-start" my={2} ml={1}>
+        {/*  <Typography alignSelf="flex-start" my={2} ml={1}>
           or
-        </Typography>
+        </Typography> */}
         <TextField
           fullWidth
           name="apikey-input"
@@ -126,34 +145,40 @@ export const Login = () => {
         />
         <Box my={2}>
           <Typography color="text.secondary" variant="body2">
-            Unsure? Choose <b>dev mode</b> and explore.
+            Unsure? Choose <b>preview mode</b> and explore.
           </Typography>
         </Box>
         <Box>
-          <Tooltip title="No maps API key required - play in development mode">
-            <Button
-              sx={{
-                my: 1,
-                mr: 2,
-              }}
-              variant="outlined"
-              color="primary"
-              onClick={handleDevMode}
-            >
-              Dev mode
-            </Button>
-          </Tooltip>
+          {isLoading ? (
+            <CircularProgress />
+          ) : (
+            <>
+              <Tooltip title="No maps API key required - play in development mode">
+                <Button
+                  sx={{
+                    my: 1,
+                    mr: 2,
+                  }}
+                  variant="outlined"
+                  color="primary"
+                  onClick={handleDevMode}
+                >
+                  Preview mode
+                </Button>
+              </Tooltip>
 
-          <Button
-            sx={{
-              my: 2,
-            }}
-            variant="contained"
-            color="primary"
-            onClick={handleSubmit}
-          >
-            Enter
-          </Button>
+              <Button
+                sx={{
+                  my: 2,
+                }}
+                variant="contained"
+                color="primary"
+                onClick={handleSubmit}
+              >
+                Enter
+              </Button>
+            </>
+          )}
         </Box>
       </Box>
       <Box position="absolute" margin="auto" bottom={30} color="text.secondary">
